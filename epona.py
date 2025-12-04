673e93175a0f28859efbd4c935b6a75410ae3c36
Samuel McIlrath <samuelmcilrath@cslab19.wheaton.edu>
2025-12-03 21:20:02 -0600

    part 1 passes

--- epona.py
+++ epona.py
@@ -4,25 +4,49 @@ from blockingdict import BlockingDict
 from ipaddress import IPv4Address, IPv4Interface, IPv4Network
 from physical import Adapter, MultiportNode, BROADCAST_MAC, MARE_PROTONUM
 
+FORMAT = 'qqh'
+
 
 class EponaAdapter(Adapter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Add any initialization you need below this line
 
-    def output(self, protonum, dst, dgram):
+    def output(self, protonum: int, dst, dgram):
         """
         Called when the network layer wishes to transmit a datagram to a
         destination host.  Provides the protocol number, destination MAC
         address, and datagram contents as bytes.
         """
+        proto_bytes = protonum.to_bytes(2, 'big')
+        checksum = get_checksum(self.hwaddr + dst + proto_bytes + dgram)
+        self.tx(self.hwaddr + dst + proto_bytes + checksum + dgram)
+        
 
-    def rx(self, frame):
+    def rx(self, frame: bytes):
         """
         Called when a frame arrives at the adapter.  Provides the frame
         contents as bytes.
         """
-        pass
+        
+        if not verify_checksum(frame):
+            #drop frame
+            print('bad checksum')
+            return
+
+        #verify you are intended reciever 
+        print(f'src hw addr {self.hwaddr}')
+        print(f'broadcast MAC: {BROADCAST_MAC}')
+        print(f'frame dest addr {frame[6:12]}')
+        print(self.hwaddr == frame[6:12])
+        
+        if self.hwaddr != frame[6:12] and frame[6:12] != BROADCAST_MAC:
+            print('wrong reciever')
+            return
+
+        print('correct recv')
+        #send on network
+        self.input(int.from_bytes(frame[12:14], 'big'), frame[15:])
 
     def output_ip(self, protonum, addr, dgram):
         """
@@ -44,3 +68,15 @@ class EponaSwitch(MultiportNode):
         an int and the frame contents as bytes.
         """
         pass
+
+
+def get_checksum(precheck: bytes):
+    """Return 1-byte checksum as bytes object."""
+    checksum_val = (~(sum(precheck) & 0xFF)) & 0xFF  # invert and mask to 1 byte
+
+    return bytes([checksum_val])
+
+def verify_checksum(segment: bytes):
+    """Verify simple 1-byte checksum."""
+    total = sum(segment) & 0xFF
+    return total == 0xFF
